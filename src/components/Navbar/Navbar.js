700959e06b900a4a593a09de4ddb1c999cbe676e
erlisakulla <erlisakulla@gmail.com>
2023-01-17 17:01:17 -0500

    Update Resume

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ import './Navbar.css';
 import { FiDownload, FiMenu, FiX } from 'react-icons/fi';
 import { Button } from 'antd';
 import { Nav } from 'react-bootstrap';
-import Resume from '../../resume/ErlisaKullaCV-Sep2021.pdf';
+import Resume from '../../resume/ErlisaKullaCV-Feb2023.pdf';
 
 function Navbar() {
   const closeNav = () => {
