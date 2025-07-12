2983a184b686001527070f2e3ac7a1f0a3ad5d45
erlisakulla <erlisakulla@gmail.com>
2025-07-12 18:23:01 +0200

    Coming soon

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './main/Home/HomePage';
-import Navbar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'antd/dist/antd.css';
 import { Helmet } from "react-helmet";
@@ -14,16 +12,11 @@ function App() {
         <title>Erlisa Kulla | Website</title>
         <meta name="description" content="Software Developer and Artist with a great passion for learning and creative exploration."/>
       </Helmet>
-
-      <Navbar/>
-
       <Router>
         <Switch>
           <Route exact path="/" component={Home}/>
         </Switch>
       </Router>
-
-      <Footer/>
     </>
   );
 }
--- src/main/Home/HomePage.js
+++ src/main/Home/HomePage.js
@@ -2,42 +2,15 @@ import React from 'react';
 import './HomePage.css';
 import self from '../../img/self.png';
 import { Grid } from '@material-ui/core';
-import { BackTop, Button } from 'antd';
-import { CanDoList } from '../sections/CanDo';
-// import { LearningList } from '../sections/Learning';
-import { Projects } from '../sections/Projects';
-import ContactMe from '../sections/ContactMe';
-import { FiChevronUp, FiChevronsRight } from 'react-icons/fi';
-import Education from '../sections/Education';
-import Experience from '../sections/Experience';
-import { FiMoreHorizontal } from 'react-icons/fi';
+// import { FiLinkedin, FiGithub, FiMail } from 'react-icons/fi';
+
 
 class Home extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.displaySkillsContent = this.displaySkillsContent.bind(this);
-    this.displayToolsContent = this.displayToolsContent.bind(this);
-
-    this.state = { 
-      cardSkillsContent: false,
-      cardToolsContent: false,
-    };
-  };
-
-  displaySkillsContent() {
-    this.setState({cardSkillsContent: !this.state.cardSkillsContent})
-  }
-
-  displayToolsContent() {
-    this.setState({cardToolsContent: !this.state.cardToolsContent})
-  }
-
   render() {
     return (
       <>
         <div className="home-page">
-          <div id="section-1">
+          <div id="intro">
             
             <Grid container spacing={1}>
               <Grid item xs={12}  md={4}>
@@ -45,87 +18,37 @@ class Home extends React.Component {
               </Grid>
 
               <Grid item xs={12} md={8}>
-                <div className="intro-text">
+                <div className="intro-section">
                   <h2 id="name">Erlisa Kulla</h2>
-                  <p id="text">
-                    <a href="https://github.com/erlisakulla" target="_blank" rel="noreferrer"><b style={{color:'#3f51b5'}} id="software">Software Developer</b></a> and <a href="https://erlisakulla.artstation.com" target="_blank" rel="noreferrer"><b style={{color:'#f50057'}} id="artist">Artist</b></a> with a great passion for learning and creative exploration.
+
+                  <p id="intro-text">
+                    <b style={{color:'#3f51b5'}} id="software">Software Developer</b> and <b style={{color:'#f50057'}} id="artist">Artist</b> with a great passion for creative exploration through programming, music and art
                   </p>
-                  <a href="#skills">
-                    <Button 
-                      type="secondary" 
-                      shape="round" 
-                      size="large"
-                      className="link-btn"
-                      icon={<FiChevronsRight size={20} style={{marginRight:'10px'}} id="icon-arrow"/>}
-                    >
-                      Get to know me
-                    </Button>
-                  </a>
+
+                  <p style={{fontSize:'20px'}} id="location">
+                    📍 Berlin, Germany
+                  </p>
+
+                  <div>
+                    <p id="coming-soon">
+                      🛠️ New Website Coming Soon...
+                    </p>
+                  </div>
+
+                  {/* <div>
+                    Resume
+                  </div> */}
+
+                  {/* <div className="social-media-icons">
+                    <a href="https://github.com/erlisakulla" target="_blank" rel="noreferrer"><FiGithub size={40}/></a>
+                    <a href="https://www.linkedin.com/in/erlisa-kulla/" target="_blank" rel="noreferrer"><FiLinkedin size={40}/></a>
+                    <a href="mailto:erlisakulla@gmail.com"><FiMail size={40}/></a>
+                  </div> */}
                 </div>
               </Grid>
             </Grid>
           </div>
-
-          <section id="skills" name="skills">
-            <h1 style={{paddingBottom:30}}>Skills</h1>
-
-            <div id="can-do">
-              {/* <h3 style={{paddingBottom:30}}>What I can do</h3> */}
-              <CanDoList/>
-            </div>
-
-            {/* <div id="learning">
-              <h3 style={{paddingBottom:30}}>What I am learning</h3>
-              <LearningList/>
-            </div> */}
-          </section>
-
-          <section id="about" name="about">
-            <h1>About Me</h1>
-            <p style={{fontSize:'16px'}} id="about-text">
-              I am from Albania, currently located in 📍 Munich, Germany. 
-              My biggest passions are programming and art and I am always looking for ways to combine the two.
-              I love 🎨 painting, 🎶 listening to music, 🎸 playing bass, 🎥 watching animated movies and shows, and of course 💻 programming.
-            </p>
-
-            <div id="experience">
-              <h3 style={{padding:30}}>Experience</h3>
-              <Experience/>
-            </div>
-
-            <div id="education">
-              <h3 style={{padding:30}}>Education</h3>
-              <Education/>
-            </div>
-          </section>
-
-          <section id="projects" name="projects">
-            <h1 style={{paddingBottom:30}}>Projects</h1>
-            <Projects/>
-            <a href="https://github.com/erlisakulla?tab=repositories" target="_blank" rel="noreferrer">
-              <Button 
-                type="secondary" 
-                shape="round" 
-                size="large"
-                id="more-btn"
-                style={{marginTop:'50px'}}
-                icon={<FiMoreHorizontal style={{marginRight:'10px'}}/>}
-              >
-                More
-              </Button>
-            </a>
-          </section>
-
-          <div id="contact" name="contact">
-            <h1 style={{fontWeight:700}}>Let's get in touch!</h1>
-            <h5 style={{paddingBottom:30, opacity:0.8}}>#OpenToWork</h5>
-            <ContactMe/>
-          </div>
         </div>
-
-        <BackTop>
-          <div><FiChevronUp id="back-top-btn" size={40}/></div>
-        </BackTop>
       </>
     );
   }
